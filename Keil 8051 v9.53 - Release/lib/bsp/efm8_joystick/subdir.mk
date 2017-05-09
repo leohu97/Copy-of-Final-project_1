8f495a2f672fee753d11d87a1eca9d0d02a32ab3
leohu97 <leohuliao1997@gmail.com>
2017-05-08 21:44:46 -0400

    Ece251-final

---
+++ Keil 8051 v9.53 - Release/lib/bsp/efm8_joystick/subdir.mk
@@ -0,0 +1,21 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.3/kits/common/drivers/efm8_joystick/joystick.c 
+
+OBJS += \
+./lib/bsp/efm8_joystick/joystick.OBJ 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+lib/bsp/efm8_joystick/joystick.OBJ: C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.0.3/kits/common/drivers/efm8_joystick/joystick.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Keil 8051 Compiler'
+	C51 "@$(patsubst %.OBJ,%.__i,$@)" || $(RC)
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Keil 8051 v9.53 - Release/src/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+A51_UPPER_SRCS += \
+../src/SILABS_STARTUP.A51 
+
+C_SRCS += \
+../src/InitDevice.c \
+../src/function_generator.c \
+../src/main.c 
+
+OBJS += \
+./src/InitDevice.OBJ \
+./src/SILABS_STARTUP.OBJ \
+./src/function_generator.OBJ \
+./src/main.OBJ 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.OBJ: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Keil 8051 Compiler'
+	C51 "@$(patsubst %.OBJ,%.__i,$@)" || $(RC)
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.OBJ: ../src/%.A51
+	@echo 'Building file: $<'
+	@echo 'Invoking: Keil 8051 Assembler'
+	AX51 "@$(patsubst %.OBJ,%.__ia,$@)" || $(RC)
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
